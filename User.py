c86afd6c4082be0b1f2c63582dd8c9afd543b674
Sandy <sandeepmandal70@gmail.com>
2020-12-16 23:29:35 +0530

    Added functionality to check if user is already available in site before performing bulk operation

--- User.py
+++ User.py
@@ -1,4 +1,5 @@
 import tableauserverclient as TSC 
+import pandas as pd
 
 def loginToSite(email,pwd,site):
     tableau_auth = TSC.TableauAuth(password=pwd,username=email,site=site)
@@ -8,23 +9,33 @@ def loginToSite(email,pwd,site):
 
 def getUserDetails(server):
     users,pagination = server.users.get()
+    # return users
     user_detail_list = list()
-
+    temp_list = set()
     for user in users:
-        temp_list = list()
-        temp_list.append(user.id)
-        temp_list.append(user.email)
-        temp_list.append(user.fullname)
-        temp_list.append(user.last_login)
-        temp_list.append(user.site_role)
+        
+        # temp_list.append(user.id)
+        # temp_list.append(user.email)
+        temp_list.add(user.name)
+        # temp_list.append(user.last_login)
+        # temp_list.append(user.site_role)
 
-        user_detail_list.append(temp_list)
-    return user_detail_list
+        # user_detail_list.append(temp_list)
+    return temp_list
     # return list(map(lambda user: user.site_role, users))
     
 
 
 if __name__ == "__main__":
-    server = loginToSite("<Your Email>","<Password>","<Site>") #enter details
-    allUser = getUserDetails(server)    
+    server = loginToSite("<Email>","<Pass>","<Site>") #enter details
+    allUser = getUserDetails(server) 
+    # print(pd.DataFrame(allUser))
+    list_to_add_user = {'a@a.com','b@b.com','y@y.com','l@l.com'}
+    # set_to_add_user = set(list_to_add_user)
+    new_user_list = allUser&list_to_add_user
+    print(new_user_list)
+    print(list_to_add_user -new_user_list )
+
     print(allUser)
+
+    
